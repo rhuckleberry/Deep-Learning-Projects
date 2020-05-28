aff9fff34073cba7baba80480a9efb93b65135d2
Ryan Huckleberry <ryanthuckleberry@gmail.com>
2020-05-28 10:36:48 -0700

    for viewing code, won't run well in this form

--- MNIST_CNN.py
+++ MNIST_CNN.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torchvision
-import torchvision.transforms as transforms
+import torchvision.transforms as transforms 
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
